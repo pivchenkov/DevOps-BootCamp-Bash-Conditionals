20acb0fcbbd5fa18e7a60d95537f3defb12e0b53
Aleksey Pivchenkov <aleksey_pivchenkov@epam.com>
2022-07-08 10:32:37 +0000

    Ready

--- cond1.sh
+++ cond1.sh
@@ -3,3 +3,14 @@
 IFS=',' read -r -a array <<< "$1"
 
 #Write your code here
+
+sum=0
+
+for num in ${array[@]}
+do
+    if [[ $((num%2)) == 0 ]]
+    then
+        sum=$((sum+num))
+    fi
+done
+echo $sum
