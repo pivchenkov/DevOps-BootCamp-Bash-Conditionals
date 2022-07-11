f7741b8d8371bdff1a87518424aa9a8a4b5f881b
Aleksey Pivchenkov <aleksey_pivchenkov@epam.com>
2022-07-11 10:10:29 +0000

    Debug lines from temp.sh removed

--- temp.sh
+++ temp.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# Place your code here
-
 input=$1
 type=${input: -1}
 temp=${input::-1}
 
 if [[ $type == "K" ]]
 then
-    echo "IF-1: Kelvin to Celsius"
     result=$(($temp-273))
     echo -n "$result"
     echo "C"
 elif [[ $type == "C" ]]
 then
-    echo "IF-2: Celsius to Kelvin"
     result=$(($temp+273))
     echo -n "$result"
     echo "K"
